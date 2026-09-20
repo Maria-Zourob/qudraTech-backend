1d5ed7f37fc31bf9d1cb6160b12ecd793eb6856e
Mariaanwar2004 <145532159+Mariaanwar2004@users.noreply.github.com>
2026-09-20 16:36:19 +0300

    Remove one-off data-fix scripts used during development

--- add-kpi.ps1
+++
@@ -1,8 +0,0 @@
-﻿$connectionString = "Server=localhost\SQLEXPRESS;Database=QudraTechDb;Trusted_Connection=True;TrustServerCertificate=True"
-$connection = New-Object System.Data.SqlClient.SqlConnection($connectionString)
-$connection.Open()
-$command = $connection.CreateCommand()
-$command.CommandText = "DELETE FROM InitiativeKpis WHERE InitiativeId = '63f5dc66-0f1a-4cf3-ad95-888d15f2b6d0'"
-$command.ExecuteNonQuery()
-$connection.Close()
-Write-Host "Old KPIs deleted"
